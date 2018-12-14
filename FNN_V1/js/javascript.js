b89f3288e09a25ae6919a3dc054699ffc78f91d8
kevinsookdeo <kevinsookdeo@users.noreply.github.com>
2018-12-14 15:58:17 -0500

    FNN_V1 Added

---
+++ FNN_V1/js/javascript.js
@@ -0,0 +1,277 @@
+//FNN Javascript
+//Testimonies
+var reviews = ["FNN was so addictive that it broke up my marriage. Thanks FNN!", "I love my FNN! It gives me everything I didn't know that I needed to know!", "That 5pm anchor is so hot, I wish I was with them...", "I hate Fake News Network so much. They're so fake that it makes me throw up.", "I watch FNN when I'm taking my exams and I haven't failed a single one yet!", "If I hear one more thing about FNN, I'll throw someone out of a window.", "That 1pm anchor is so ugly, it looks like someone ran over them with a steamroller.", "I told my granddaughter to put it on FNN. She put it on the Fake News Network. I wanted Fox News!", "You all make me sick.", "When I become president, I'm going to get rid of the Fake News Network!", "Da Da Da Da Da...Hey! Da Da Da Da. Da Da Da Da Da...Hey! Da Da.", "My cable company got into a dispute with every other media company except for Fake News Network, so now FNN is all I watch! I'm not crazy at all!", "I hate you all.", "I love you all!", "My wife loves the Fake News Network and I don't know why. Maybe she's crazy.", "I AM TYPING IN ALL CAPITAL LETTERS!!!", "I AM SCREAMING!", "LOOK HOW LOUDLY I'M TALKING", "I thought I was getting paid to give my opinion on FNN and it turns out that THEY WANT FOR FREE...lowlives"];
+var person2 = ["Billy", "Susie", "Kim", "John", "Tiberius", "Rex", "Clifford", "Arnold", "Lucy", "Marie", "Frank", "Stormi", "Aalisha", "Abbagail", "Seven", "Fork", "Addisen", "Africa", "Ahmod", "Ahrianna", "Rupert", "Homer", "Al", "Kaitlin", "Aesha", "Bucky", "Byren", "Curry", "Dick", "Jude", "Philip"];
+    //https://www.parents.com/baby-names/ used for some names
+var worldlocations = ["Brooklyn, New York", "New York, New York", "Albany, New York", "Los Angeles, California", "Flin Flon, Canada", "Kingston, Jamaica", "Tiajuana, Mexico", "Amsterdam, Netherlands", "Tokyo, Japan", "Toronto, Canada", "Shanghai, China", "Seoul, South Korea", "Havana, Cuba", "Pyongyang, North Korea", "New Delhi, India", "Hyderabad, India", "Karachi, Pakistan", "Yokohama, Japan", "Paris, France", "Jakarta, Indonesia", "Bogota, Colombia", "Guangzhou, China", "Lahore, Pakistan", "Yangon, Myanmar", "Saint Petersburg, Russia", "Johannesburg, South Africa", "Austin, Texas", "Biloxi, Mississippi", "Fayetteville, North Carolina"];
+    //Apple Maps consulted for World Locations
+
+//Coming up on FNN
+var senses = ["Learn", "See", "Hear", "Find out"];
+var actiontease = ["a football player tackled", "a chef prepared", "a mother dropped", "a frog kissed", "a TV fell on", "investigators solved the mystery behind", "a college professor gave students", "multiple people ate", "a person fell on"];
+var thing2 = ["frog", "football", "baby", "person", "banana", "bucket full of sand", "pogo stick", "honeymooning couple", "couple on a first date", "couple on a second date", "jealous ex", "muffin", "grass", "ventriloquist and their dummy"];
+var finalaction = ["survived", "died", "got into an unusual type of accident", "got fired", "became president", "became vice-president", "got gonorrhea", "didn't die", "published an article on FNN"];
+var worldlocations0 = ["Brooklyn, New York", "New York, New York", "Albany, New York", "Los Angeles, California", "Flin Flon, Canada", "Kingston, Jamaica", "Tiajuana, Mexico", "Amsterdam, Netherlands", "Tokyo, Japan", "Toronto, Canada", "Shanghai, China", "Seoul, South Korea", "Havana, Cuba", "Pyongyang, North Korea", "New Delhi, India", "Hyderabad, India", "Karachi, Pakistan", "Yokohama, Japan", "Paris, France", "Jakarta, Indonesia", "Bogota, Colombia", "Guangzhou, China", "Lahore, Pakistan", "Yangon, Myanmar", "Saint Petersburg, Russia", "Johannesburg, South Africa", "Austin, Texas", "Biloxi, Mississippi", "Fayetteville, North Carolina"];
+    //Apple Maps consulted for World Locations
+    //Duplicate-1; for different carousel stories
+var senses1 = ["Learn", "See", "Hear", "Find out"];
+var actiontease1 = ["a football player tackled", "a chef prepared", "a mother dropped", "a frog kissed", "a TV fell on", "investigators solved the mystery behind", "a college professor gave students", "multiple people ate", "a person fell on"];
+var thing21 = ["frog", "football", "baby", "person", "banana", "bucket full of sand", "pogo stick", "honeymooning couple", "couple on a first date", "couple on a second date", "jealous ex", "muffin", "grass", "ventriloquist and their dummy"];
+var finalaction1 = ["survived", "died", "got into an unusual type of accident", "got fired", "became president", "became vice-president", "got gonorrhea", "didn't die", "published an article on FNN"];
+var worldlocations1 = ["Brooklyn, New York", "New York, New York", "Albany, New York", "Los Angeles, California", "Flin Flon, Canada", "Kingston, Jamaica", "Tiajuana, Mexico", "Amsterdam, Netherlands", "Tokyo, Japan", "Toronto, Canada", "Shanghai, China", "Seoul, South Korea", "Havana, Cuba", "Pyongyang, North Korea", "New Delhi, India", "Hyderabad, India", "Karachi, Pakistan", "Yokohama, Japan", "Paris, France", "Jakarta, Indonesia", "Bogota, Colombia", "Guangzhou, China", "Lahore, Pakistan", "Yangon, Myanmar", "Saint Petersburg, Russia", "Johannesburg, South Africa", "Austin, Texas", "Biloxi, Mississippi", "Fayetteville, North Carolina"];
+    //Apple Maps consulted for World Locations
+    //Duplicate-2; for different carousel stories
+var senses2 = ["Learn", "See", "Hear", "Find out"];
+var actiontease2 = ["a football player tackled", "a chef prepared", "a mother dropped", "a frog kissed", "a TV fell on", "investigators solved the mystery behind", "a college professor gave students", "multiple people ate", "a person fell on"];
+var thing22 = ["frog", "football", "baby", "person", "banana", "bucket full of sand", "pogo stick", "honeymooning couple", "couple on a first date", "couple on a second date", "jealous ex", "muffin", "grass", "ventriloquist and their dummy"];
+var finalaction2 = ["survived", "died", "got into an unusual type of accident", "got fired", "became president", "became vice-president", "got gonorrhea", "didn't die", "published an article on FNN"];
+var worldlocations2 = ["Brooklyn, New York", "New York, New York", "Albany, New York", "Los Angeles, California", "Flin Flon, Canada", "Kingston, Jamaica", "Tiajuana, Mexico", "Amsterdam, Netherlands", "Tokyo, Japan", "Toronto, Canada", "Shanghai, China", "Seoul, South Korea", "Havana, Cuba", "Pyongyang, North Korea", "New Delhi, India", "Hyderabad, India", "Karachi, Pakistan", "Yokohama, Japan", "Paris, France", "Jakarta, Indonesia", "Bogota, Colombia", "Guangzhou, China", "Lahore, Pakistan", "Yangon, Myanmar", "Saint Petersburg, Russia", "Johannesburg, South Africa", "Austin, Texas", "Biloxi, Mississippi", "Fayetteville, North Carolina"];
+    //Apple Maps consulted for World Locations
+    //Duplicate-3; for different carousel stories
+var senses3 = ["Learn", "See", "Hear", "Find out"];
+var actiontease3 = ["a football player tackled", "a chef prepared", "a mother dropped", "a frog kissed", "a TV fell on", "investigators solved the mystery behind", "a college professor gave students", "multiple people ate", "a person fell on"];
+var thing23 = ["frog", "football", "baby", "person", "banana", "bucket full of sand", "pogo stick", "honeymooning couple", "couple on a first date", "couple on a second date", "jealous ex", "muffin", "grass", "ventriloquist and their dummy"];
+var finalaction3 = ["survived", "died", "got into an unusual type of accident", "got fired", "became president", "became vice-president", "got gonorrhea", "didn't die", "published an article on FNN"];
+var worldlocations3 = ["Brooklyn, New York", "New York, New York", "Albany, New York", "Los Angeles, California", "Flin Flon, Canada", "Kingston, Jamaica", "Tiajuana, Mexico", "Amsterdam, Netherlands", "Tokyo, Japan", "Toronto, Canada", "Shanghai, China", "Seoul, South Korea", "Havana, Cuba", "Pyongyang, North Korea", "New Delhi, India", "Hyderabad, India", "Karachi, Pakistan", "Yokohama, Japan", "Paris, France", "Jakarta, Indonesia", "Bogota, Colombia", "Guangzhou, China", "Lahore, Pakistan", "Yangon, Myanmar", "Saint Petersburg, Russia", "Johannesburg, South Africa", "Austin, Texas", "Biloxi, Mississippi", "Fayetteville, North Carolina"];
+    //Apple Maps consulted for World Locations
+
+//Entertainment
+var emotion = ["Scared", "Flustered", "Sad", "Ashamed", "Dumbfounded", "Idiotic", "Surprised"];
+var occupation = ["professional people pusher", "boyfriend", "girlfriend", "sleeper", "vomit cleaner", "sniffer", "fisherman"];
+var action = ["perform", "give", "buy", "sell", "make", "cook", "deliver", "wash", "sound the alarm against", "boil crayons to make", "pass a sobriety test for"];
+var thing = ["some money", "some cookies", "some cucumbers", "a box", "a bacon, egg and cheese sandwich", "canned unicorn meat", "a pair of neck underwear", "the end of humanity"];
+var natureNoun = ["in the ocean", "on the mountain", "in the forest", "with the clouds", "while floating down a river", "while jumping off of a tree", "jumping towards the sky", "while leaving earth", "while crossing the desert", "while falling out of a space shuttle in outer space"];
+
+//Opinion
+var person1 = ["The dentist", "The carver", "The bridesmaid", "The butcher", "The TV host"];
+var verb = ["expect", "want", "need"];
+var blank = ["understand advanced Calculus", "create a new theory of relativity", "operate a camera", "cook some food", "rate the meal", "sleep on the bed", "perform 100 pushups, 150 situps and 200 squats"];
+var person3 = ["the cracker salter", "the bed tester", "the person sleeping on the job", "the chef", "the bride", "the groom", "the jealous boyfriend", "the jealous girlfriend"];
+var authorize = ["authorized", "not authorized"];
+
+var blank2 = ["perform a lobotomy", "film a honeymoon", "cook a table", "boil a shoe", "clean the ceiling", "stick a screwdriver into that outlet over  there", "test a bed", "put salt on crackers"];
+var taster = ["pet food taster", "cracker taster", "cheese taster"];
+var doctor = ["physician", "surgeon", "registered nurse", "pharmacist", "gynecologist", "dentist", "technician"];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+//Testimonies
+var person22 = choice(person2);
+var world = choice(worldlocations);
+var review = choice(reviews);
+
+//Coming up on FNN
+var sense = choice(senses);
+var actiontea = choice(actiontease);
+var thing3 = choice(thing2);
+var finalact = choice(finalaction);
+var world0 = choice(worldlocations);
+    //Duplicate 1
+var sense1 = choice(senses);
+var actiontea1 = choice(actiontease);
+var thing31 = choice(thing2);
+var finalact1 = choice(finalaction);
+var world1 = choice(worldlocations);
+    //Duplicate 2
+var sense2 = choice(senses2);
+var actiontea2 = choice(actiontease);
+var thing32 = choice(thing2);
+var finalact2 = choice(finalaction);
+var world2 = choice(worldlocations);
+    //Duplicate 3
+var sense3 = choice(senses3);
+var actiontea3 = choice(actiontease);
+var thing33 = choice(thing2);
+var finalact3 = choice(finalaction);
+var world3 = choice(worldlocations);
+
+//Local News
+var sense4 = choice(senses);
+var actiontea4 = choice(actiontease);
+var thing34 = choice(thing2);
+var finalact4 = choice(finalaction);
+var world04 = choice(worldlocations);
+
+var emotions4 = choice(emotion);
+var occupations4 = choice(occupation);
+var actions4 = choice(action);
+var things4 = choice(thing);
+var natureNouns4 = choice(natureNoun);
+
+var person114 = choice(person1);
+var verbs4 = choice(verb);
+var blanks4 = choice(blank);
+var person334 = choice(person3);
+var authorized4 = choice(authorize);
+
+var person115 = choice(person1);
+var verbs45 = choice(verb);
+var blanks5 = choice(blank);
+var person335 = choice(person3);
+var authorized5 = choice(authorize);
+
+//World News
+var sense40 = choice(senses);
+var actiontea40 = choice(actiontease);
+var thing340 = choice(thing2);
+var finalact40 = choice(finalaction);
+var world040 = choice(worldlocations);
+
+var emotions40 = choice(emotion);
+var occupations40 = choice(occupation);
+var actions40 = choice(action);
+var things40 = choice(thing);
+var natureNouns40 = choice(natureNoun);
+
+var person1140 = choice(person1);
+var verbs40 = choice(verb);
+var blanks40 = choice(blank);
+var person3340 = choice(person3);
+var authorized40 = choice(authorize);
+
+var person1150 = choice(person1);
+var verbs450 = choice(verb);
+var blanks50 = choice(blank);
+var person3350 = choice(person3);
+var authorized50 = choice(authorize);
+
+//Entertainment
+var emotions = choice(emotion);
+var occupations = choice(occupation);
+var actions = choice(action);
+var things = choice(thing);
+var natureNouns = choice(natureNoun);
+    //Duplicate 1
+var emotions1 = choice(emotion);
+var occupations1 = choice(occupation);
+var actions1 = choice(action);
+var things1 = choice(thing);
+var natureNouns1 = choice(natureNoun);
+    //Duplicate 2
+var emotions2 = choice(emotion);
+var occupations2 = choice(occupation);
+var actions2 = choice(action);
+var things2 = choice(thing);
+var natureNouns2 = choice(natureNoun);
+    //Duplicate 3
+var emotions3 = choice(emotion);
+var occupations3 = choice(occupation);
+var actions3 = choice(action);
+var things3 = choice(thing);
+var natureNouns3 = choice(natureNoun);
+
+//Opinion
+var person11 = choice(person1);
+var verbs = choice(verb);
+var blanks = choice(blank);
+var person33 = choice(person3);
+var authorized = choice(authorize);
+
+var person111 = choice(person1);
+var verbs1 = choice(verb);
+var blanks1 = choice(blank);
+var person331 = choice(person3);
+var authorized1 = choice(authorize);
+
+var person12 = choice(person1);
+var verbs2 = choice(verb);
+var blanks2 = choice(blank);
+var person332 = choice(person3);
+var authorized2 = choice(authorize);
+
+var person113 = choice(person1);
+var verbs3 = choice(verb);
+var blanks3 = choice(blank);
+var person333 = choice(person3);
+var authorized3 = choice(authorize);
+
+//Numbers-Pictures
+var number1 = choice(numbers);
+var number2 = choice(numbers);
+var number3 = choice(numbers);
+var number4 = choice(numbers);
+var number5 = choice(numbers);
+var number6 = choice(numbers);
+var number7 = choice(numbers);
+var number8 = choice(numbers);
+
+var blank22 = choice(blank2);
+var tasters = choice(taster);
+var doctors = choice(doctor);
+$("img.image1").attr("src", "img/NewsPictures/News/" + number1 + ".jpeg");
+$("img.image2").attr("src", "img/NewsPictures/News/" + number2 + ".jpeg");
+$("img.image3").attr("src", "img/NewsPictures/News/" + number3 + ".jpeg");
+$("img.image4").attr("src", "img/NewsPictures/News/" + number4 + ".jpeg");
+$("img.image5").attr("src", "img/NewsPictures/News/" + number5 + ".jpeg");
+$("img.image6").attr("src", "img/NewsPictures/News/" + number6 + ".jpeg");
+$("img.image7").attr("src", "img/NewsPictures/News/" + number7 + ".jpeg");
+$("img.image8").attr("src", "img/NewsPictures/News/" + number8 + ".jpeg");
+
+$("img.image11").attr("src", "img/NewsPictures/Entertainment/" + number1 + ".jpeg");
+$("img.image12").attr("src", "img/NewsPictures/Entertainment/" + number2 + ".jpeg");
+$("img.image13").attr("src", "img/NewsPictures/Entertainment/" + number3 + ".jpeg");
+$("img.image14").attr("src", "img/NewsPictures/Entertainment/" + number4 + ".jpeg");
+
+$("img.image21").attr("src", "img/NewsPictures/Opinion/" + number1 + ".jpeg");
+$("img.image22").attr("src", "img/NewsPictures/Opinion/" + number2 + ".jpeg");
+$("img.image23").attr("src", "img/NewsPictures/Opinion/" + number3 + ".jpeg");
+$("img.image24").attr("src", "img/NewsPictures/Opinion/" + number4 + ".jpeg");
+
+$("h4.card-text").text(review);
+$("p.card-title").text(person22 + " from " + world);
+$("img.testimonies").attr("src", "img/faces/" + person22 + ".jpeg");
+$("h5.carouselstory1").text(sense + " how " + actiontea + " a " + thing3 + " and " + finalact);
+$("h5.carouselstory2").text(sense1 + " how " + actiontea1 + " a " + thing31 + " and " + finalact1);
+$("h5.carouselstory3").text(sense2 + " how " + actiontea2 + " a " + thing32 + " and " + finalact2);
+$("h5.carouselstory4").text(sense3 + " how " + actiontea3 + " a " + thing33 + " and " + finalact3);
+$("p.location1").text("Location: " + world0);
+$("p.location2").text("Location: " + world1);
+$("p.location3").text("Location: " + world2);
+$("p.location4").text("Location: " + world3);
+
+$("p.localnews1").text(sense4 + " how " + actiontea4 + " a " + thing34 + " and " + finalact4);
+$("p.localnews2").text(emotions4 + " " + occupations4 + " is unable to " + actions4 + " " + things4 + " " + natureNouns4);
+$("p.localnews3").text(person114 + " doesn't " + verbs4 + " " + person334 + " to " + blanks4);
+$("p.localnews4").text(person115 + " doesn't " + verbs45 + " " + person335 + " to " + blanks5);
+
+$("p.worldnews2").text(sense40 + " how " + actiontea40 + " a " + thing340 + " and " + finalact40);
+$("p.worldnews3").text(emotions40 + " " + occupations40 + " is unable to " + actions40 + " " + things40 + " " + natureNouns40);
+$("p.worldnews4").text(person1140 + " doesn't " + verbs40 + " " + person3340 + " to " + blanks40);
+$("p.worldnews1").text(person1150 + " doesn't " + verbs450 + " " + person3350 + " to " + blanks50);
+
+$("p.entertainment1").text(emotions + " " + occupations + " is unable to " + actions + " " + things + " " + natureNouns);
+$("p.entertainment2").text(emotions1 + " " + occupations1 + " is unable to " + actions1 + " " + things1 + " " + natureNouns1);
+$("p.entertainment3").text(emotions2 + " " + occupations2 + " is unable to " + actions2 + " " + things2 + " " + natureNouns2);
+$("p.entertainment4").text(emotions3 + " " + occupations3 + " is unable to " + actions3 + " " + things3 + " " + natureNouns3);
+    
+$("p.opinion1").text(person111 + " doesn't " + verbs1 + " " + person331 + " to " + blanks1);
+$("p.opinion2").text(person12 + " is " + authorized2 + " to " + blanks2);
+$("p.opinion3").text(person113 + " doesn't " + verbs3 + " " + person333 + " to " + blanks3);
+$("p.opinion4").text(person11 + " is " + authorized + " to " + blanks);
+
+
+function myFunction() {
+    var x = document.getElementById("demo")
+    x.innerHTML = Math.floor((Math.random() * 100) + 1);
+}
+
+
+
+    
+var topics = ["FlyerChat", "Food Central", "Computer Land", "Safari Barn", "Hunter Foods", "Gourmet Concrete"];
+var photos = ["https://cdn.thinglink.me/api/image/535133207088070658/1240/10/scaletowidth", "https://d1u1mce87gyfbn.cloudfront.net/media/thumbnail/hanzo-gameplay.jpg", "https://www.rd.com/wp-content/uploads/2016/01/01-rethink-what-you-drink-faucet.jpg" ];
+
+
+function choice(list) {
+    return list[Math.floor(Math.random() * list.length)];
+};
+
+
+var topic = choice(topics);
+$('h1').text(topic + " " + "Central");
+$('h3').addClass("title");
+$('.topic').text(topic);
+var images = choice(photos);
+$('img').attr("src", image);
+
+
